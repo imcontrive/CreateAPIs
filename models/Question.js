512ea8b273b445f5abe71661e32ce8c01ca413f2
shubh <psubham94@gmail.com>
2019-06-14 10:40:40 +0530

    edit Questions model

--- models/Question.js
+++ models/Question.js
@@ -2,15 +2,20 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var questionSchema = new Schema({
-  question: {
-    type: String,
-    default: "",
+  questions: {
+    question:{
+      type: String,
+      default: "",
+    },
+    options: {
+        type: Object,
+        default: ""
+      },
+    description: {
+      type: String,
+      default: ""
+    }
   },
-  options: {
-    type: Object,
-    default: ""
-  },
-  description: String
 }, {timestamps: true})
 
 module.exports = mongoose.model('Question', questionSchema);
