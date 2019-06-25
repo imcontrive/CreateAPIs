36cf3734fdd362594ce4165f504c72d8d26644a0
shubh <psubham94@gmail.com>
2019-06-25 23:09:00 +0530

    modified Home.js component

--- client/mini-codewar-frontend/src/components/Home.js
+++ client/mini-codewar-frontend/src/components/Home.js
@@ -6,8 +6,6 @@ class Home extends Component {
 			<div>
 				<div className="hero" style={{ width:"100%",height:"600px",backgroundSize:"100% 100%",backgroundImage: "url(" + "/media/raw.jpg" + ")"}}>
 					<h3 style={{bottom:"-135px",left: "276.5px",textAlign: "center",fontWeight: "800",fontStyle: "italic",textDecoration: "none",fontFamily: "Lato",fontSize: "50px",color: "rgb(255, 255, 255)",margin: "0px",zIndex: "auto",width: "798px",height: "240px",padding: "0px",borderWidth: "0px",borderRadius: "0px",letterSpacing:"0px",position:"relative"}}>Alternative to Codewars <small style={{display:"block", fontSize:"20px"}}>Anyone Can Practice and Perfect their Programming Skills.</small></h3>
-					{/* It is a place built for anyone to practice and perfect their programming skills. Challenges are added almost every day so you can work on applying your..Practice and Perfect their Programming Skills. */}
-
 				</div>
 			</div>
 		);
