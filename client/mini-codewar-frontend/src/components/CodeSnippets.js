1087cbdda1c6ae17bd22f0a4766f76a8766bf71f
shubh <psubham94@gmail.com>
2019-06-27 12:10:11 +0530

    CodeSnippets.js

---
+++ client/mini-codewar-frontend/src/components/CodeSnippets.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CodeSnippets extends Component {
+  render() {
+    return (
+      <div>
+        code snippets
+      </div>
+    )
+  }
+}
