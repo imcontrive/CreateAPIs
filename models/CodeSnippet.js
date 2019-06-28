ac1a60f354b512f26f88ce199c63f72115b52af7
shubh <psubham94@gmail.com>
2019-06-28 09:53:46 +0530

    codeSnippetsSchema

---
+++ models/CodeSnippet.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var codeSnippetsSchema = new Schema({
+  questions: [{
+    question:{
+      type: String,
+      default: "",
+    },
+    iframe: {
+      type: String,
+      default:""
+    },
+    options: {
+        type: Object,
+        default: ""
+      },
+    answer: {
+      type: Object,
+    },
+    description: {
+      type: String,
+      default: ""
+    }
+  }],
+}, {timestamps: true})
+
+module.exports = mongoose.model('CodeSnippets', codeSnippetsSchema);
