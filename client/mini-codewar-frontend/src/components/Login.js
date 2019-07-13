613daf7632e5553217f48f2b92cf7a61fa62d1b0
shubh <psubham94@gmail.com>
2019-07-13 16:08:24 +0530

    client/mini-codewar-frontend/src/components/Login.js

--- client/mini-codewar-frontend/src/components/Login.js
+++ client/mini-codewar-frontend/src/components/Login.js
@@ -41,9 +41,6 @@ class Login extends Component {
 	render() {
 		return (
 			<div className='login'>
-				{/* <div className="hero" style={{ width:"100%",height:"600px",backgroundSize:"100% 100%",backgroundImage: "url(" + "/media/raw.jpg" + ")"}}>
-					<h3 style={{bottom:"-135px",left: "276.5px",textAlign: "center",fontWeight: "800",fontStyle: "italic",textDecoration: "none",fontFamily: "Lato",fontSize: "50px",color: "rgb(255, 255, 255)",margin: "0px",zIndex: "auto",width: "798px",height: "240px",padding: "0px",borderWidth: "0px",borderRadius: "0px",letterSpacing:"0px",position:"relative"}}>Alternative to Codewars <small style={{display:"block", fontSize:"20px"}}>Anyone Can Practice and Perfect their Programming Skills.</small></h3>
-				</div> */}
 				<form>
 				  <h2>Login</h2>
 				  <input onChange={this.handleChange} name='email' placeholder='E-Mail Address' type='text' value={ this.state.user.email } required/>
