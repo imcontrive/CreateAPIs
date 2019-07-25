f57db9ea8c93e935e528a386dba566129990ff79
shubh <psubham94@gmail.com>
2019-07-25 11:07:35 +0530

    update compiler

--- client/mini-codewar-frontend/src/components/CodeMirror.js
+++ client/mini-codewar-frontend/src/components/CodeMirror.js
@@ -10,6 +10,7 @@ var CodeEditor = require('codemirror');
        returnValue: ""
      }
      this.myCodeMirror = null;
+    this.codeMirrorStr = "function xyz(){ \n \n // \t write code \n \n }\n \n xyz();";
    }
 
   componentDidMount() {
@@ -26,14 +27,16 @@ var CodeEditor = require('codemirror');
   }
 
   // HandleClick function
+  // this.codeMirrorStr = `function xyz(){}`
     handleClick = () => {
-      var codeMirrorStr = this.myCodeMirror.getValue();
+      // var codeMirrorStr = this.myCodeMirror.getValue();
       try{
-        this.setState({returnValue : eval(codeMirrorStr)});
+        this.setState({returnValue : eval(this.codeMirrorStr)});
       } catch(err){
+      
         console.log(err);
       }
-      console.log(this.state.returnValue,"checkpoint 1");
+      // console.log(this.state.returnValue,"checkpoint 1");
       // if(returnValue == 6) {
       //   alert('right answer')
       // } else {
@@ -43,7 +46,11 @@ var CodeEditor = require('codemirror');
   render() {
     return (
       <React.Fragment>
-          <div className="container" style={{minHeight:"565px",height:"100%"}}>
+        <div className="quesBox" style={{width:"565px",margin:"0 auto", padding:"40px"}}>
+          <div className="QuesTitle"style={{color:"red"}}>Square(n) Sum</div>
+          
+        </div>
+        <div className="container" style={{minHeight:"565px",height:"100%"}}>
             <div className="row clearfloat">
               <div className="side col-md-12">
                 <div style={{textAlign:"center"}} className="editor col-md-4">
@@ -60,7 +67,7 @@ var CodeEditor = require('codemirror');
 
               </div>
               <div className="editor col-md-8">
-                <textarea id="codejs">
+                <textarea id="codejs">{this.codeMirrorStr ? this.codeMirrorStr : ""}
                 </textarea>
                 <button id="run" className="btn btn-primary" onClick={this.handleClick}>Run</button>
               </div>
