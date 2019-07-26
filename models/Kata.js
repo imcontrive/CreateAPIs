3ad37d155304babb990a48bcf5301459bf0fe5f4
shubh <psubham94@gmail.com>
2019-07-26 10:34:20 +0530

    create kata Schema

---
+++ models/Kata.js
@@ -0,0 +1,35 @@
+// Require mongoose:
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//create the instance of schema
+var kataSchema = new Schema({
+  kata: {
+    type: String,
+    default: "",
+  },
+  title: {
+    type: String,
+    default: ""
+  },
+  level:{
+    type: String,
+    default: ""
+  },
+  description: {
+    type: String,
+    default: ""
+  },
+  code: {
+    type: String,
+    default: ""
+  },
+  testCase: {
+    type: String,
+    default: ""
+  },
+}, {timestamps: true})
+
+
+
+module.exports = mongoose.model('Kata', kataSchema);
