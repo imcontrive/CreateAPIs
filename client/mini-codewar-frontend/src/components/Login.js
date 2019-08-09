6f8a987a09423c2f78bcef1cbdd95941f93c7b82
shubh <psubham94@gmail.com>
2019-08-09 13:33:41 +0530

    Login

--- client/mini-codewar-frontend/src/components/Login.js
+++ client/mini-codewar-frontend/src/components/Login.js
@@ -40,18 +40,6 @@ class Login extends Component {
 
 	render() {
 		return (
-			/* <div className='isLoginWrapper'>
-				<form className='login'>
-				  <h2>Login</h2>
-				  <input onChange={this.handleChange} name='email' placeholder='E-Mail Address' type='text' value={ this.state.user.email } required/>
-				  <input onChange={this.handleChange} id='pw' name='password' placeholder='Password' type='password' value={ this.state.user.password } required/>
-				  <button type="button" classNameName="btn btn-info" onClick={this.handleLogin}>Login</button>
-				</form>
-			  <div className="login-flex">
-			  	<p className='forgot'>Don't have an account?</p>
-			  	<Link to="/register">Register</Link>
-			  </div>
-			</div> */ 
 			<div  className="page-wrapper">
 				<div  className="text-center">
 					<img  alt="" src="http://www.cakecounter.com/img/swagatam.png"/>	
@@ -65,17 +53,17 @@ class Login extends Component {
 						{/* Form Data */}
 						<form className="theme-form ng-untouched ng-pristine ng-valid">
 							<div className="form-group">
-								<label  className="col-form-label pt-0">Email</label>
+								<label class="col-form-label pt-0">Email</label>
 								<input  className="form-control ng-untouched ng-pristine ng-valid" formcontrolname="email" onChange={this.handleChange} name='email' type="email"  value={ this.state.user.email }required/>
 							</div>
 							<div className="form-group">
 								<label  className="col-form-label">Password</label>
 								<input  className="form-control ng-untouched ng-pristine ng-valid" formcontrolname="password"  name='password' onChange={this.handleChange} type="password" value={ this.state.user.password }required/>
 							</div>
-							<div  className="checkbox p-0">
+							{/* <div  className="checkbox p-0">
 								<input  id="checkbox1" type="checkbox"/>
 								<label  for="checkbox1">Remember me</label>
-							</div>
+							</div> */}
 							<div  className="form-group form-row mt-3 mb-0">
 								<button  className="btn btn-primary btn-block btn-lg" type="submit" onClick={this.handleLogin}><span>Login</span></button>
 								<div className="notExist">
